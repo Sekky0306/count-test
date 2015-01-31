10e4b61155e6969edfcd94f1d7bec1fd815b487d
Sekky <step1336@gmail.com>
2015-01-31 12:27:34 +0900

    Initial Commit

---
+++ count/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  count
+//
+//  Created by 関戸優紀 on 2015/01/31.
+//  Copyright (c) 2015年 関戸優紀. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
